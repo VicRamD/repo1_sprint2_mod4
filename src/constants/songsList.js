c387b03bbad11414a712a0a6f8ac8b4b7b1b9bbe
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-03-26 19:58:21 -0300

    moviendo imagenes a public

--- src/constants/songsList.js
+++ src/constants/songsList.js
@@ -1,14 +1,14 @@
 export const songsList = [
-    {id: 0, title: "I'll be there for you", alt:"I'll be there for you cover", cover: "/src/assets/songsImg/ill-be-there-for-you-cover.png"},
-    {id: 1, title: "Anthesis", alt:"Anthesis cover", cover: "/src/assets/songsImg/anthesis-cover.jpg"},
-    {id: 2, title: "All star", alt:"All star cover", cover: "/src/assets/songsImg/all-star-cover.png"},
-    {id: 3, title: "It's my life", alt:"It's my life cover", cover: "/src/assets/songsImg/its-my-life-cover.jpg"},
-    {id: 4, title: "No pain, no game", alt:"No pain, no pain cover", cover: "/src/assets/songsImg/no-pain-no-game-cover.png"},
-    {id: 5, title: "The crossing", alt:"The crossing cover", cover: "/src/assets/songsImg/the-crossing-cover.jpg"},
-    {id: 6, title: "The final countdown", alt:"The final countdown cover", cover: "/src/assets/songsImg/the-final-countdown-cover.jpg"},
-    {id: 7, title: "The wheekend whip", alt:"The wheekend whip cover", cover: "/src/assets/songsImg/the-weekend-whip-cover.png"},
-    {id: 8, title: "Let it be", alt:"Let it be cover", cover: "/src/assets/songsImg/let-it-be-cover.jpg"},
-    {id: 9, title: "Don't stop me now", alt:"Don't stop me now cover", cover: "/src/assets/songsImg/dont-stop-me-now-cover.jpg"},
-    {id: 10, title: "Who am I?", alt:"Who am I cover", cover: "/src/assets/songsImg/who-am-I-cover.jpg"},
-    {id: 11, title: "Wind of change", alt:"Wind of change cover", cover: "/src/assets/songsImg/wind-of-change-cover.jpg"}
+    {id: 0, title: "I'll be there for you", alt:"I'll be there for you cover", cover: "/songsImg/ill-be-there-for-you-cover.png"},
+    {id: 1, title: "Anthesis", alt:"Anthesis cover", cover: "/songsImg/anthesis-cover.jpg"},
+    {id: 2, title: "All star", alt:"All star cover", cover: "/songsImg/all-star-cover.png"},
+    {id: 3, title: "It's my life", alt:"It's my life cover", cover: "/songsImg/its-my-life-cover.jpg"},
+    {id: 4, title: "No pain, no game", alt:"No pain, no pain cover", cover: "/songsImg/no-pain-no-game-cover.png"},
+    {id: 5, title: "The crossing", alt:"The crossing cover", cover: "/songsImg/the-crossing-cover.jpg"},
+    {id: 6, title: "The final countdown", alt:"The final countdown cover", cover: "/songsImg/the-final-countdown-cover.jpg"},
+    {id: 7, title: "The wheekend whip", alt:"The wheekend whip cover", cover: "/songsImg/the-weekend-whip-cover.png"},
+    {id: 8, title: "Let it be", alt:"Let it be cover", cover: "/songsImg/let-it-be-cover.jpg"},
+    {id: 9, title: "Don't stop me now", alt:"Don't stop me now cover", cover: "/songsImg/dont-stop-me-now-cover.jpg"},
+    {id: 10, title: "Who am I?", alt:"Who am I cover", cover: "/songsImg/who-am-I-cover.jpg"},
+    {id: 11, title: "Wind of change", alt:"Wind of change cover", cover: "/songsImg/wind-of-change-cover.jpg"}
 ]
