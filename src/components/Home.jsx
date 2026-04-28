1ca861ee76fe48866f6a2dccf6d69735732ee4a4
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-04-28 17:55:32 -0300

    modificación a estilos de home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,13 @@ import logo from '../assets/sz-logo-home.png'
 const Home = () => {
   return (
     <main className="pt-16">
-        <div className='flex w-full'>
-            <img src={logo} alt="Logo de Sound Zone" />
+        <div className='flex w-full justify-center'>
+            <img src={logo} alt="Logo de Sound Zone" className='size-64'/>
         </div>
         <h1 className='text-center font-bold text-4xl'>Escucha tus canciones favoritas</h1>
+        <div>
+            
+        </div>
     </main>
   )
 }
